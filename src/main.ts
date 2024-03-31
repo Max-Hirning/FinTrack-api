dd88895489cac2f16e4e914ce7d6ad79312c5279
Max-Hirning <maxhirning25@gmail.com>
2024-03-31 10:39:25 +0300

    Returned prev cors config and added headers in vercel.json

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,19 @@ import {v2 as cloudinary} from 'cloudinary';
 import {ValidationPipe} from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   cloudinary.config({
     api_key: process.env.CLOUDINARY_APIKEY,
     cloud_name: process.env.CLOUDINARY_CLOUDNAME,
     api_secret: process.env.CLOUDINARY_APISECRET,
+  });
+  app.enableCors({
+    origin: [
+      process.env.ORIGIN_URL,
+      'http://localhost:3000',
+    ],
+    methods: 'GET, PUT, POST, DELETE, OPTIONS',
   });
   await app.listen(process.env.PORT);
   // eslint-disable-next-line no-console
